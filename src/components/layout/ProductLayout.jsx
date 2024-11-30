ef0c4e6f46e984b7b3f56b5e8cbb90021557714b
shakib0079 <shakib100630@gmail.com>
2024-11-30 16:46:37 +0600

    Feat: Product details page developed

---
+++ src/components/layout/ProductLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router"
+
+function ProductLayout() {
+  return (
+    <>
+        <Outlet />
+    </>
+  )
+}
+
+export default ProductLayout
--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -4,6 +4,10 @@ import Products from "../Products";
 import Contact from "../Contact";
 import About from "../About";
 import Navbar from "../Navbar";
+import ProductDetails from "../ProductDetails";
+import ProductLayout from "./ProductLayout";
+
+
 
 const divStyle = {
     display: "flex",
@@ -19,7 +23,10 @@ function RootLayout() {
         <div style={divStyle}>
         <Routes>
             <Route index element={<Home />}/>
-            <Route path="products" element={<Products />}/>
+            <Route path="products" element={<ProductLayout />}>
+              <Route index element={<Products/>}/>
+              <Route path=":id" element={<ProductDetails/>}/>
+            </Route>
             <Route path="contact" element={<Contact />}/>
             <Route path="about" element={<About />}/>
         </Routes>
